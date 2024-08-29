29d104d1f5170dfe9fb730adbf68d84c7547b258
kimepo <step33az@gmail.com>
2024-08-30 02:28:31 +0300

    1

--- StaticWebScraping/Program.cs
+++ StaticWebScraping/Program.cs
@@ -21,37 +21,49 @@ namespace WebScraping
             // downloading to the target page
             // and parsing its HTML content
             var document = web.Load(url);
-            var nodes = document.DocumentNode.SelectNodes("//*[@id='tableTrades']");
+            var nodes = document.DocumentNode.SelectNodes("//*[@id='tableTrades']");//*[@id="WebPartWPQ1"]/div[1]/div[2]
             List<Trades> Trades = new List<Trades>();
-            foreach ( var node in nodes )
+            try
             {
-                Trades.Add(new Trades()
+                foreach (var node in nodes)
                 {
-                    TradeDate = HtmlEntity.DeEntitize(node.SelectSingleNode("th[1]").InnerText),
+                    Trades.Add(new Trades()
 
-                    TradeType = HtmlEntity.DeEntitize(node.SelectSingleNode("td[2]").InnerText),
+                    {
+                        TradeDate = HtmlEntity.DeEntitize(node.SelectSingleNode("th[1]").InnerText),
 
-                    ShortName = HtmlEntity.DeEntitize(node.SelectSingleNode("td[3]").InnerText),
+                        TradeType = HtmlEntity.DeEntitize(node.SelectSingleNode("td[2]").InnerText),
 
-                    FutureExpiry = HtmlEntity.DeEntitize(node.SelectSingleNode("td[4]").InnerText),
+                        ShortName = HtmlEntity.DeEntitize(node.SelectSingleNode("td[3]").InnerText),
 
-                    Strike = HtmlEntity.DeEntitize(node.SelectSingleNode("td[5]").InnerText),
+                        FutureExpiry = HtmlEntity.DeEntitize(node.SelectSingleNode("td[4]").InnerText),
 
-                    CallPut = HtmlEntity.DeEntitize(node.SelectSingleNode("td[6]").InnerText),
+                        Strike = HtmlEntity.DeEntitize(node.SelectSingleNode("td[5]").InnerText),
 
-                    Quantity = HtmlEntity.DeEntitize(node.SelectSingleNode("td[7]").InnerText),
+                        CallPut = HtmlEntity.DeEntitize(node.SelectSingleNode("td[6]").InnerText),
 
-                    Vol = HtmlEntity.DeEntitize(node.SelectSingleNode("td[8]").InnerText),
+                        Quantity = HtmlEntity.DeEntitize(node.SelectSingleNode("td[7]").InnerText),
 
-                    Premium = HtmlEntity.DeEntitize(node.SelectSingleNode("td[9]").InnerText),
+                        Vol = HtmlEntity.DeEntitize(node.SelectSingleNode("td[8]").InnerText),
 
-                    FuturesPrice = HtmlEntity.DeEntitize(node.SelectSingleNode("td[10]").InnerText)
+                        Premium = HtmlEntity.DeEntitize(node.SelectSingleNode("td[9]").InnerText),
 
-                });
+                        FuturesPrice = HtmlEntity.DeEntitize(node.SelectSingleNode("td[10]").InnerText)
+
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally 
+            {
+                Console.WriteLine(Trades);
             }
 
-            using (var writer = new StreamWriter("infoTrade.csv"))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            var writer = new StreamWriter("infoTrade.csv" + DateTime.Today);
+            var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
             {
                 csv.WriteRecords(Trades);
             }
