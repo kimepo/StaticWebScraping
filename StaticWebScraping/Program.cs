6ef21ef7e06b199b1e4fe36ead7355eb1c4a68f3
kimepo <step33az@gmail.com>
2024-09-11 13:41:39 +0300

    1

--- StaticWebScraping/Program.cs
+++ StaticWebScraping/Program.cs
@@ -1,5 +1,6 @@
-﻿using HtmlAgilityPack;
-using StaticWebScraping;
+﻿using StaticWebScraping;
+using System.Net.Http.Json;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
@@ -7,67 +8,93 @@ using CsvHelper;
 using System.IO;
 using System.Text;
 using System.Globalization;
+using Newtonsoft.Json;
 
 namespace WebScraping
 {
-    public class program
+    class program
     {
-        public static void Main()
+       static void Main(string[] args)
         {
-            string url =
-                "https://clientportal.jse.co.za/reports/delta-option-and-structured-option-trades";
-
-            var web = new HtmlWeb();
-            // downloading to the target page
-            // and parsing its HTML content
-            var document = web.Load(url);
-            var nodes = document.DocumentNode.SelectNodes("//*[@id='tableTrades']");//*[@id="WebPartWPQ1"]/div[1]/div[2]
-            List<Trades> Trades = new List<Trades>();
             try
             {
-                foreach (var node in nodes)
+                var resultat = GetData(url: "https://clientportal.jse.co.za/reports/silo-unavailability-report");
+                if (resultat != null) 
                 {
-                    Trades.Add(new Trades()
-
+                    Console.WriteLine(string.Join("\t", resultat));
+                    foreach (var row in resultat.Property1) 
                     {
-                        TradeDate = HtmlEntity.DeEntitize(node.SelectSingleNode("th[1]").InnerText),
-
-                        TradeType = HtmlEntity.DeEntitize(node.SelectSingleNode("td[2]").InnerText),
-
-                        ShortName = HtmlEntity.DeEntitize(node.SelectSingleNode("td[3]").InnerText),
-
-                        FutureExpiry = HtmlEntity.DeEntitize(node.SelectSingleNode("td[4]").InnerText),
-
-                        Strike = HtmlEntity.DeEntitize(node.SelectSingleNode("td[5]").InnerText),
-
-                        CallPut = HtmlEntity.DeEntitize(node.SelectSingleNode("td[6]").InnerText),
-
-                        Quantity = HtmlEntity.DeEntitize(node.SelectSingleNode("td[7]").InnerText),
-
-                        Vol = HtmlEntity.DeEntitize(node.SelectSingleNode("td[8]").InnerText),
-
-                        Premium = HtmlEntity.DeEntitize(node.SelectSingleNode("td[9]").InnerText),
-
-                        FuturesPrice = HtmlEntity.DeEntitize(node.SelectSingleNode("td[10]").InnerText)
-
-                    });
+                        Console.WriteLine(string.Join("\t",row));
+                    }
                 }
+                Console.ReadKey();
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            finally 
-            {
-                Console.WriteLine(Trades);
-            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
 
-            var writer = new StreamWriter("infoTrade.csv" + DateTime.Today);
-            var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-            {
-                csv.WriteRecords(Trades);
-            }
+            //var writer = new StreamWriter("infoTrade.csv" + DateTime.Today);
+            //var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            //{
+            //    csv.WriteRecords(Trades);
+            //}
 
         }
+
+        private static Trades GetData(string url)
+        {
+            try 
+            {
+                using (HttpClientHandler hdl = new HttpClientHandler 
+                {
+                    AllowAutoRedirect = false,
+                    AutomaticDecompression = System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.None,
+                    CookieContainer = new System.Net.CookieContainer() 
+                }) 
+                {
+                    using (HttpClient clnt = new HttpClient(hdl, false)) 
+                    {
+                        clnt.DefaultRequestHeaders.Add("User-Agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0");
+                        clnt.DefaultRequestHeaders.Add("Accept:", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+                        clnt.DefaultRequestHeaders.Add("Accept-Language:", "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7");
+                        //clnt.DefaultRequestHeaders.Add("Connection", "");
+                        clnt.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests:", "1");
+                        //clnt.DefaultRequestHeaders.Add("Accept-Encoding:", "gzip, deflate, br, zstd");
+
+
+                        using (var resp = clnt.GetAsync(url).Result) 
+                        {
+                            if (!resp.IsSuccessStatusCode) 
+                            {
+                                return null;
+                            }
+                        }
+                    }
+
+                    using (HttpClient clnt = new HttpClient(hdl, false))
+                    {
+                        clnt.DefaultRequestHeaders.Add("User-Agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0");
+                        clnt.DefaultRequestHeaders.Add("Accept:", "application/json, text/javascript, */*; q=0.01");
+                        clnt.DefaultRequestHeaders.Add("Accept-Language:", "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7");
+
+                        clnt.DefaultRequestHeaders.Add("X-Requested-With::", "XMLHttpRequest");
+                        clnt.DefaultRequestHeaders.Add("Referer:", url);
+
+                        using (var resp = clnt.GetAsync("https://clientportal.jse.co.za/_vti_bin/JSE/CommoditiesService.svc/GetSiloData").Result)
+                        {
+                            var json = resp.Content.ReadAsStringAsync().Result;
+                            if(!string.IsNullOrEmpty(json))
+                            {
+                                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<Trades>(json);
+                                return result;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            return null;
+        }
     }
+
 }
--- StaticWebScraping/Trades.cs
+++ StaticWebScraping/Trades.cs
@@ -4,20 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StaticWebScraping {
+namespace StaticWebScraping
+{
 
     public class Trades
     {
-        public string TradeDate { get; set; }
-        public string TradeType { get; set; }
-        public string ShortName { get; set; }
-        public string FutureExpiry { get; set; }
-        public string Strike { get; set; }
-        public string CallPut { get; set; }
-        public string Quantity { get; set; }
-        public string Vol { get; set; }
-        public string Premium { get; set; }
-        public string FuturesPrice { get; set; }
+
+        
+            public Class1[] Property1 { get; set; }
+    }
+
+        public class Class1
+        {
+            public string Comment { get; set; }
+            public DateTime FromDate { get; set; }
+            public int ID { get; set; }
+            public string Location { get; set; }
+            public string Operator { get; set; }
+            public string Reason { get; set; }
+            public string Region { get; set; }
+            public DateTime ToDate { get; set; }
+        }
+
+
 
     }
-}
+
